e36fad254ab47228c2c175a3a538b44f3635102d
tnwl2126 <51833394+tnwl2126@users.noreply.github.com>
2020-06-30 12:50:09 +0900

    update todo

--- src/gameTest/Character.java
+++ src/gameTest/Character.java
@@ -54,45 +54,45 @@ public class Character {
 		
 	}
 	
-	public void itemGenerate(Character character) {
-	
-		String name="", type="";
-		int buildUpCount=0;
-		
-		
-		System.out.print("캐릭터 이름 : ");
-		character.mName = scan.next();
-		//System.out.println("캐릭터 레벨 : " + character.mLevel);
-		System.out.println("사용할 아이템 속성을 입력해 주세요.");
-		System.out.print("아이템 이름 : ");
-		name = scan.next();
-		System.out.print("아이템 종류(무기인가 옷인가 등) : ");
-		type = scan.next();
-		System.out.print("강화할 수 있는 횟수 : ");
-		buildUpCount = scan.nextInt();
-		
-		Item item = new Item(name, type, buildUpCount);
-		character.mItem.add(item);
-	}
-	
-	public void skillGenerate(Character character) {
-		
-		String name="", type="";
-		int hp=0, np = 0;
-		
-		
-		System.out.print("스킬 이름 : ");
-		name = scan.next();
-		System.out.print("스킬 종류 : ");
-		type = scan.next();
-		System.out.print("hp : ");
-		hp = scan.nextInt();
-		System.out.print("np : ");
-		np = scan.nextInt();
-		
-		Skill skills = new Skill(name,type,hp,np);
-		character.mSkills.add(skills);
-	}
-	
+//	public void itemGenerate(Character character) {
+//	
+//		String name="", type="";
+//		int buildUpCount=0;
+//		
+//		
+//		System.out.print("캐릭터 이름 : ");
+//		character.mName = scan.next();
+//		//System.out.println("캐릭터 레벨 : " + character.mLevel);
+//		System.out.println("사용할 아이템 속성을 입력해 주세요.");
+//		System.out.print("아이템 이름 : ");
+//		name = scan.next();
+//		System.out.print("아이템 종류(무기인가 옷인가 등) : ");
+//		type = scan.next();
+//		System.out.print("강화할 수 있는 횟수 : ");
+//		buildUpCount = scan.nextInt();
+//		
+//		Item item = new Item(name, type, buildUpCount);
+//		character.mItem.add(item);
+//	}
+//	
+//	public void skillGenerate(Character character) {
+//		
+//		String name="", type="";
+//		int hp=0, np = 0;
+//		
+//		
+//		System.out.print("스킬 이름 : ");
+//		name = scan.next();
+//		System.out.print("스킬 종류 : ");
+//		type = scan.next();
+//		System.out.print("hp : ");
+//		hp = scan.nextInt();
+//		System.out.print("np : ");
+//		np = scan.nextInt();
+//		
+//		Skill skills = new Skill(name,type,hp,np);
+//		character.mSkills.add(skills);
+//	}
+//	
 
 }
